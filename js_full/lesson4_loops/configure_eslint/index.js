28ac22a7c7e6727d2abb88cf3e79c2dad18fb336
vnSasa <ol.yaremechko@gmail.com>
2026-03-22 09:38:37 +0200

    added js_full configure_eslint

--- js_full/lesson4_loops/configure_eslint/index.js
+++ js_full/lesson4_loops/configure_eslint/index.js
@@ -1,8 +1,15 @@
 function run() {
-    var a = 0,
-        b = 1;
+  var a = 0;
 
-    if (a == b) {
-        return c;
-    }
+  if (a == 1) {
+    return c;
+  }
+
+  for (let i = 1; i < 10; i++) {
+    console.log(i);
+  }
+
+  return (d = a);
 }
+
+run();
