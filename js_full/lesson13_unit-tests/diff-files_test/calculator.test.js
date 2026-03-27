9128100c270ec8498f682c4f472582e1c448a20b
vnSasa <ol.yaremechko@gmail.com>
2026-03-27 08:22:19 +0200

    added js_full lesson_unit-tests diff-files_test

---
+++ js_full/lesson13_unit-tests/diff-files_test/calculator.test.js
@@ -0,0 +1,19 @@
+import getSum, { getOddNumbers, getSquareNumber } from './calculator.js';
+
+it('should get square number', () => {
+  const result = getSquareNumber([1, 2, 3]);
+
+  expect(result).toEqual([1, 4, 9]);
+});
+
+it('should keep odd numbers only', () => {
+  const result = getOddNumbers([1, 2, 3, 4, 5]);
+
+  expect(result).toEqual([1, 3, 5]);
+});
+
+it('should get sum of numbers', () => {
+  const result = getSum(8, 4);
+
+  expect(result).toEqual(12);
+});
