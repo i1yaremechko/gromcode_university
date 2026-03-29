82e450c9b2313ef2f99b417c51ded77166467408
vnSasa <ol.yaremechko@gmail.com>
2026-03-30 00:07:28 +0300

    added js_full lesson_dom search_elems

--- js_full/lesson21_dom/search_elements/index.js
+++ js_full/lesson21_dom/search_elements/index.js
@@ -1,11 +1,11 @@
 export const getTitleElement = () => {
   const titleElem = document.querySelector('.title');
   console.dir(titleElem);
-  return titleElem.textContent;
+  return titleElem;
 };
 
 export const getInputElement = () => {
   const inputElem = document.querySelector('input[type="text"]');
   console.dir(inputElem);
-  return inputElem.value;
+  return inputElem;
 };
