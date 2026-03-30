1743c4793ead8699087883b695b120fb6ffce500
vnSasa <ol.yaremechko@gmail.com>
2026-03-30 10:16:02 +0300

    added js_full lesson_dom add_atributes f

--- js_full/lesson21_dom/add_atributes/index.js
+++ js_full/lesson21_dom/add_atributes/index.js
@@ -1,14 +1,13 @@
-const finishForm = () => {
+export const finishForm = () => {
   const formElem = document.querySelector('.login-form');
-
-  const typeElem = formElem.querySelector('input[type="text"]');
-  typeElem.type = 'password';
 
   const loginElem = document.createElement('input');
   loginElem.type = 'text';
   loginElem.name = 'login';
   formElem.prepend(loginElem);
+
+  const typeElem = formElem.querySelector('input[name="password"]');
+  typeElem.type = 'password';
 };
 
 finishForm();
-
