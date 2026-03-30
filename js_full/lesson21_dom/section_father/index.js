5fe5e5fa4c38ad7a9bcd515aec1a949cc727e00b
vnSasa <ol.yaremechko@gmail.com>
2026-03-30 13:46:31 +0300

    added js_full lesson_dom get_section dataset

--- js_full/lesson21_dom/section_father/index.js
+++ js_full/lesson21_dom/section_father/index.js
@@ -1,4 +1,8 @@
 export const getSection = (num) => {
   const spanElem = document.querySelector(`span[data-number="${num}"]`);
-  return spanElem.closest('div[data-section]');
+  const section = spanElem.closest('.box');
+
+  return String(section.dataset.section);
 };
+
+console.log(getSection(6));
