0ed0cd8a4b77531a42c5249b3289b48472dbc5c9
vnSasa <ol.yaremechko@gmail.com>
2026-03-30 12:57:13 +0300

    added js_full lesson_dom data-attributess

--- js_full/lesson21_dom/data_attributes/index.js
+++ js_full/lesson21_dom/data_attributes/index.js
@@ -1,7 +1,11 @@
 export const squaredNumbers = () => {
   const numElem = document.querySelectorAll('.number');
-  Array.from(numElem).forEach((elem) => {
+  const squareArr = Array.from(numElem).map((elem) => {
     const number = Number(elem.dataset.number);
-    elem.dataset.squareNumber = number * number;
+    const square = number * number;
+
+    elem.dataset.sqareNumber = square;
+    return String(square);
   });
+  return squareArr;
 };
