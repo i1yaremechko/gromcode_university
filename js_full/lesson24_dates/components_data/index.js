d651899fa62f14c0c24368eb9fd7e40b0a780444
vnSasa <ol.yaremechko@gmail.com>
2026-04-01 12:40:40 +0300

    added js_full lesson_date components_data

---
+++ js_full/lesson24_dates/components_data/index.js
@@ -0,0 +1,12 @@
+const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+
+const dayOfWeek = (date, days) => {
+  const day = new Date(date).getDate();
+  const dateInFuture = new Date(date).setDate(day + days);
+
+  return weekDays[new Date(dateInFuture).getDay()];
+};
+
+const result = dayOfWeek(new Date(2026, 4, 1), 14);
+
+console.log(result);
