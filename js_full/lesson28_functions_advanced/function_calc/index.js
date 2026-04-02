4e2121504a22352f83cc9fdeebdc6f62e173692d
vnSasa <ol.yaremechko@gmail.com>
2026-04-02 15:59:10 +0300

    lesson_28 functions_advanced calc

---
+++ js_full/lesson28_functions_advanced/function_calc/index.js
@@ -0,0 +1,27 @@
+const calc = (number) => {
+  let result = number;
+
+  const calculator = {
+    add(number) {
+      result += number;
+      return this;
+    },
+    substract(number) {
+      result -= number;
+      return this;
+    },
+    mult(number) {
+      result *= number;
+      return this;
+    },
+    div(number) {
+      result /= number;
+      return this;
+    },
+    result() {
+      return result;
+    },
+  };
+
+  return calculator;
+};
