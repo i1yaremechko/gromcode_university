5ee6cb49901a379412f66b2409f184da70875f2c
vnSasa <ol.yaremechko@gmail.com>
2026-04-02 12:48:46 +0300

    lesson_27 local_storage data

---
+++ js_full/lesson27_locale-storage/data_local-storage/index.js
@@ -0,0 +1,15 @@
+const getLocalStorageData = () => {
+  return Object.entries(localStorage)
+    .reduce((acc, [key, value]) => {
+      let newValue;
+      try {
+        newValue = JSON.parse(value);
+      } catch (error) {
+        newValue = value;
+      }
+      return {
+        ...acc,
+        [key]: newValue,
+      };
+    }, {});
+};
