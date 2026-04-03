30aa050b511da5882b1ecfddaaabf185f19f5e43
vnSasa <ol.yaremechko@gmail.com>
2026-04-03 11:48:49 +0300

    lesson_29 asynchronous load_imп

--- js_full/lesson29_asynchronous/load_image/index.js
+++ js_full/lesson29_asynchronous/load_image/index.js
@@ -1,21 +1,14 @@
 export const addImage = (imgSrc, callback) => {
-  const containerElem = document.querySelector('.page');
   const imgElem = document.createElement('img');
-
   imgElem.setAttribute('alt', 'Photo');
   imgElem.src = imgSrc;
+  const containerElem = document.querySelector('.page');
   containerElem.append(imgElem);
 
-  const onImageLoaded = () => {
-    const { width, height } = imgElem;
-    callback(null, { width, height });
-  };
-
-  imgElem.addEventListener('load', onImageLoaded);
+  imgElem.addEventListener('load', () => callback(null, imgElem));
   imgElem.addEventListener('error', () => callback('Image load is failed'));
 };
 
-// callack function
 const onImageLoaded = (error, imgElem) => {
   if (error) {
     console.log(error);
