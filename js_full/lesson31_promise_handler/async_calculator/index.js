7b745493f71ef637d92cf218b10d08aafdda185c
vnSasa <ol.yaremechko@gmail.com>
2026-04-04 00:15:10 +0300

    lesson_31 promise_handler async

---
+++ js_full/lesson31_promise_handler/async_calculator/index.js
@@ -0,0 +1,21 @@
+const asyncCalculator = (numbr) => new Promise((resolve) => {
+  setTimeout(() => {
+    console.log(`Initial value: ${numbr}`)
+    resolve(numbr);
+  }, 500);
+})
+.then(value => new Promise((resolve) => {
+    setTimeout(() => {
+      const square = value * value;
+      console.log(`Square value: ${square}`);
+      resolve(square);
+    }, 500);
+  })
+)
+.then(value => {
+  const doubleSquare = value * 2;
+  console.log(`Double value: ${doubleSquare}`);
+  return doubleSquare;
+})
+
+asyncCalculator(5);
