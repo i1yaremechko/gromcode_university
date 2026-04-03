b006be6789a36073ea434ec9399e2dce5905f0b4
vnSasa <ol.yaremechko@gmail.com>
2026-04-03 13:29:00 +0300

    lesson_29 asynchronous interval

---
+++ js_full/lesson29_asynchronous/interval/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} count
+ * @param {number} period
+ * @return {undefined}
+ */
+export const pinger = (count, period) => {
+  let i = count;
+  console.log('Ping');
+  const interval = setInterval(() => {
+    if (--i > 0) {
+      console.log('Ping');
+    } else {
+      clearInterval(interval);
+    }
+  }, period);
+};
