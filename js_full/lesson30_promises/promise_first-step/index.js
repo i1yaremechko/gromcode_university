c7bdab68d347bff7d3c2943dfb027f2db52adfa5
vnSasa <ol.yaremechko@gmail.com>
2026-04-03 15:02:51 +0300

    lesson_30 promises first_step

---
+++ js_full/lesson30_promises/promise_first-step/index.js
@@ -0,0 +1,25 @@
+export const addImage = (imgSrc) => {
+  const p = new Promise((resolve, reject) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'Photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    const onImageLoaded = () => {
+      const { width, height } = imgElem;
+      resolve({ width, height });
+    };
+
+    imgElem.addEventListener('load', () => onImageLoaded);
+    imgElem.addEventListener('error', () => reject(new Error('Image load is failed')));
+  });
+
+  return p;
+};
+
+const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+
+const resultPromise = addImage(imgSrc);
+
+resultPromise.then(data => console.log(data));
