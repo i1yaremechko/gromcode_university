eaa6c7502db200110f165a7aad459e325a19bd25
vnSasa <ol.yaremechko@gmail.com>
2026-04-03 11:32:56 +0300

    lesson_29 asynchronous load_image

---
+++ js_full/lesson29_asynchronous/load_image/index.js
@@ -0,0 +1,35 @@
+const addImage = (imgSrc, callback) => {
+  const containerElem = document.querySelector('.page');
+  const imgElem = document.createElement('img');
+
+  imgElem.setAttribute('alt', 'Photo');
+  imgElem.src = imgSrc;
+  containerElem.append(imgElem);
+
+  const onImageLoaded = () => {
+    const { width, height } = imgElem;
+    callback(null, { width, height });
+  };
+
+  imgElem.addEventListener('load', onImageLoaded);
+  imgElem.addEventListener('error', () => callback('Image load is failed'));
+};
+
+// callack function
+const onImageLoaded = (error, imgElem) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const { width, height } = imgElem;
+  const sizeElem = document.querySelector('.image-size');
+
+  sizeElem.textContent = `${width} x ${height}`;
+};
+
+// examples
+addImage(
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+  onImageLoaded,
+);
