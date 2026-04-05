efd05062cbead57e9c8495b21de5034007a8ccc5
vnSasa <ol.yaremechko@gmail.com>
2026-04-05 15:39:24 +0300

    lesson_33 connect_http get-request

---
+++ js_full/lesson33_connect_http/get_request/scripts/index.js
@@ -0,0 +1,27 @@
+const userAvatarElem = document.querySelector('.user__avatar');
+const userNameElem = document.querySelector('.user__name');
+const userLocationElem = document.querySelector('.user__location');
+
+const userNameInputElem = document.querySelector('.name-form__input');
+const getNameFormBtnElem = document.querySelector('.name-form__btn');
+
+const fetchUserData = userName =>
+  fetch(`http://api.github.com/users/${userName}`)
+    .then(response => response.json());
+
+const renderUserData = userData => {
+  const { avatar_url, name, location } = userData;
+  userAvatarElem.src = avatar_url;
+  userNameElem.textContent = name;
+  userLocationElem.textContent = location
+    ? `from ${location}`
+    : '';
+};
+
+const onSearchUser = () => {
+  const userName = userNameInputElem.value;
+  fetchUserData(userName)
+    .then(userData => renderUserData(userData));
+};
+
+getNameFormBtnElem.addEventListener('click', onSearchUser);
