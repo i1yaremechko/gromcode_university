5db504b7922b1075ffa6dd5b2abb364a08efc28b
vnSasa <ol.yaremechko@gmail.com>
2026-04-06 15:14:51 +0300

    lesson_36 promise_advanced get_user aaa

--- js_full/lesson36_promise-advanced/get_user/index.js
+++ js_full/lesson36_promise-advanced/get_user/index.js
@@ -1,12 +1,12 @@
 export const fetchUser = async userId => {
   try {
-    const response = await fetch(`https://api.githum.com/users/${userId}`);
+    const response = await fetch(`https://api.github.com/users/${userId}`);
     if (!response.ok) {
       return null;
     }
     const userData = await response.json();
     return userData;
   } catch (error) {
-    throw new Error('Failed to fetch user');
+    throw new Error('Failed to get user data');
   }
 };
