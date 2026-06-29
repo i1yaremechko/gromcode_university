faf5bb7f8e1b6d2a1f81a8e1742166afb6ae72cd
vnSasa <ol.yaremechko@gmail.com>
2026-06-29 17:24:24 +0300

    added lesson-2 jsx-render-elements

---
+++ react/lesson-2/jsx-render-elements/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1>Search Form</h1>
+      <div>
+        <input type="text" />
+        <button>Search</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
