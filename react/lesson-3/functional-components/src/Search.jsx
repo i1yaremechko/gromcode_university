0f60d9f8521f96c4f1093185a23799c5c0800b7b
vnSasa <ol.yaremechko@gmail.com>
2026-06-30 00:15:40 +0300

    added lesson-2 functional-components tom

--- react/lesson-3/functional-components/src/Search.jsx
+++ react/lesson-3/functional-components/src/Search.jsx
@@ -1,12 +1,14 @@
-function Search ({name}) {
+function Search ({ name }) {
   return (
-    <div className="search">
-      <h1 className="search__title">Hello, {name}. What to search for you?</h1>
-      <div className="search__field">
-        <input className="search__input" type="text" />
-        <button className="search__button">Search</button>
+    <>
+      <div className="search">
+        <h1 className="search__title">Hello, {name}. What to search for you?</h1>
+        <div className="search__field">
+          <input className="search__input" type="text" />
+          <button className="search__button">Search</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
