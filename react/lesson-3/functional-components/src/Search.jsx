e3165a03234417ef4ca7782ade03d9f879443669
vnSasa <ol.yaremechko@gmail.com>
2026-06-30 00:20:06 +0300

    added lesson-2 functional-component tom

--- react/lesson-3/functional-components/src/Search.jsx
+++ react/lesson-3/functional-components/src/Search.jsx
@@ -1,8 +1,8 @@
-function Search ({ name }) {
+function Search (props) {
   return (
     <>
       <div className="search">
-        <h1 className="search__title">Hello, {name}. What to search for you?</h1>
+        <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
         <div className="search__field">
           <input className="search__input" type="text" />
           <button className="search__button">Search</button>
