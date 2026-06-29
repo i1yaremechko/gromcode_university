95f6daf276239cb680a45dab22f3ee578fae0837
vnSasa <ol.yaremechko@gmail.com>
2026-06-29 17:51:38 +0300

    added lesson-2 jsx-attributes

---
+++ react/lesson-2/jsx-attributes/src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  const styleSpan = {color: 'red', fontWeight: 700};
+  
+  return (
+    <main className="page">
+      <form className="login-form">
+        <h1 className="form-title">Profile</h1>
+        <div className="form-control">
+          <label className="form-label" htmlFor="name">Name</label>
+          <input className="form-input" type="text" id="name" value="anonymous" />
+        </div>
+        <div className="form-control">
+          <label className="form-label" id="age" htmlFor="age">Age</label>
+          <input className="form-input" type="number" value="17" name="age" />
+          <span style={styleSpan}>To young</span>
+        </div>
+        <button className="submit-button" type="submit">Submit</button>
+      </form>
+    </main>
+  );
+}
+
+export default App;
