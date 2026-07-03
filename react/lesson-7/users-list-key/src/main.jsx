0932e5d035898b2a61491c2c3b99ef2c480bb978
vnSasa <ol.yaremechko@gmail.com>
2026-07-03 11:08:58 +0300

    added lesson-7 users-list-key

---
+++ react/lesson-7/users-list-key/src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import UsersList from './UsersList.jsx';
+
+const rootElement = document.getElementById('root');
+
+const users = [
+  {
+    id: 'id-0',
+    age: 21,
+    name: 'Bob',
+  },
+  {
+    id: 'id-1',
+    age: 17,
+    name: 'Tom',
+  },
+  {
+    id: 'id-2',
+    age: 18,
+    name: 'Tad',
+  },
+  {
+    id: 'id-3',
+    age: 45,
+    name: 'Sam',
+  },
+];
+
+ReactDOM.render(
+  <React.Fragment>
+    <UsersList users={users} />
+  </React.Fragment>,
+  rootElement
+);
