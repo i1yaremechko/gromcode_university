9e28074fa0be249516f6b4369db69a98788ba2db
vnSasa <ol.yaremechko@gmail.com>
2026-07-07 15:09:30 +0300

    added lesson-11 update-component

---
+++ react/lesson-11/update-component/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import EvenNumbers from './EvenNumbers';
+import Numbers from './Number';
+import OddNumbers from './OddNumbers';
+
+class App extends React.Component {
+  state = {
+    number: 0,
+  };
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState({
+        number: this.state.number + 1
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <OddNumbers title='Odd numbers' number={this.state.number} />
+        <Numbers title='All numbers' number={this.state.number} />
+        <EvenNumbers title='Even numbers' number={this.state.number} />
+      </div>
+    );
+  }
+}
+
+export default App;
