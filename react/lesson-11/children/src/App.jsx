9f529d49876e6c40eb5d6fb02986bd2b677f8a31
vnSasa <ol.yaremechko@gmail.com>
2026-07-07 16:00:03 +0300

    added lesson-11 children

---
+++ react/lesson-11/children/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Dialog from './Dialog';
+
+class App extends React.Component {
+  state = {
+    isOpen: false,
+  }
+
+  hideDialog = () => {
+    this.setState({
+      isOpen: false,
+    });
+  };
+
+  showDialog = () => {
+    this.setState({
+      isOpen: true,
+    });
+  };
+  
+  render() {
+    return (
+      <div className='app'>
+        <button className='btn' onClick={this.showDialog}>
+          Show
+        </button>
+        <Dialog 
+          isOpen={this.state.isOpen} 
+          onClose={this.hideDialog}
+          title="some title"
+        >
+          <p>Some text</p>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default App;
