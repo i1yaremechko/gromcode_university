35fca568f6b49a1110ee43b58d33253742d6b93f
vnSasa <ol.yaremechko@gmail.com>
2026-07-09 15:55:04 +0300

    added lesson-14 user-data

---
+++ react/lesson-14/users-data/src/User.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+React.version;
+
+const User = ({ match }) => {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${match.params.userId}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error();
+      })
+      .then((userData) => {
+        setUserData(userData);
+      });
+  }, [match.params.userId]);
+
+  if (!userData) return null;
+  const { name, location, avatar_url } = userData;
+
+  return (
+    <div className="user">
+      <img
+        alt="User Avatar"
+        src={avatar_url}
+        className="user__avatar"
+      />
+      <div className="user__info">
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
+      </div>
+    </div>
+  );
+}
+
+export default User;
