968b3c475928c8f9d00e0d8beb2b61998c296008
vnSasa <ol.yaremechko@gmail.com>
2026-07-09 16:39:34 +0300

    added lesson-14 dimensions

---
+++ react/lesson-14/window-dimensions/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dimensions from './Dimensions.jsx';
+import './index.scss';
+React.version;
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<Dimensions />, rootElement);
